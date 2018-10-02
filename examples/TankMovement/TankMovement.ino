c658e9960b126c3d3bcc3b7224b111d72adf69fb
Phillip Vea <phillipvea2408@gmail.com>
2018-10-02 11:00:14 -0400

    Added tank example

---
+++ examples/TankMovement/TankMovement.ino
@@ -0,0 +1,21 @@
+#include <DFRTank.h>
+
+DFRTank tank;
+
+void setup(){
+  Serial.begin(9600);
+  tank.init();
+}
+void loop(){
+  //Move forward
+  tank.setLeftMotorPWM(255);
+  tank.setRightMotorPWM(255);
+  delay(500);
+  //Turn left
+  tank.setLeftMotorPWM(-255);
+  tank.setRightMotorPWM(255);
+  delay(500);
+  //Stop
+  tank.turnOffMotors();
+  delay(500);
+}
