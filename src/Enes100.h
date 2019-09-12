482491ab610ec15703d7c707aa986147dfd44f30
Jet Lee <gamestar109@live.com>
2019-09-12 12:49:32 -0400

    Add new objectives to Enes100.h

--- src/Enes100.h
+++ src/Enes100.h
@@ -3,18 +3,23 @@
 
 #include "VisionSystemClient.hpp"
 
-#define BLACK_BOX   0
-#define DATA        1
-#define DEBRIS      2
-#define FIRE        3
-#define WATER       4
+#define BLACK_BOX         0
+#define DATA              1
+#define DEBRIS            2
+#define FIRE              3
+#define WATER             4
 
-#define FRESH       0
-#define POLLUTED    1
-#define SALT        2
+#define RED               0
+#define GREEN             1
+#define BLUE              2
 
-#define COPPER      0
-#define STEEL       1
+#define FRESH_UNPOLLUTED  0
+#define FRESH_POLLUTED    1
+#define SALT_UNPOLLUTED   2
+#define SALT_POLLUTED     3
+
+#define PLASTIC           0
+#define ALUMINUM          1
 
 VisionSystemClient Enes100;
 
