1884652470abc9dfadbe899f37dc695c9cded687
nperkin3 <nperkin3@umd.edu>
2019-09-13 15:20:51 -0400

    Initial Commit

---
+++ examples/DATA/data.ino
@@ -0,0 +1,35 @@
+#include "Enes100.h"
+
+void setup() {
+    // Initialize Enes100 Library
+    // Team Name, Mission Type, Marker ID, TX Pin, RX Pin
+    Enes100.begin("Data Mission", Data, 3, 8, 9);
+
+    //Print out Coordinates of the destination
+    Enes100.print("Destination is at (");
+    Enes100.print(Enes100.destination.x);
+    Enes100.print(", ");
+    Enes100.print(Enes100.destination.y);
+    Enes100.println(")");
+
+    // Any other setup code...
+}
+
+void loop() {
+    // Update the OSV's current location
+    if (Enes100.updateLocation()) {
+        Enes100.print("OSV is at (");
+        Enes100.print(Enes100.location.x);
+        Enes100.print(", ");
+        Enes100.print(Enes100.location.y);
+        Enes100.print(", ");
+        Enes100.print(Enes100.location.theta);
+        Enes100.println(")");
+    } else {
+        // OSV's location was not found
+        Enes100.println("404 Not Found");
+    }
+
+    // Transmit the Message
+    Enes100.mission("A");
+}
