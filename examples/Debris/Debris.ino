e03798147d4bf1fe200c63f69b48da64928ac482
nperkin3 <nperkin3@umd.edu>
2019-09-13 15:23:56 -0400

    Changed Material from steel to plastic

--- examples/Debris/Debris.ino
+++ examples/Debris/Debris.ino
@@ -30,8 +30,8 @@ void loop() {
         Enes100.println("404 Not Found");
     }
 
-    // Transmit the material of the debris
-    Enes100.mission(STEEL);
+    // Transmit the material of the debris (Plastic or Aluminum)
+    Enes100.mission(Plastic);
 
     // Transmit the mass of the debris in grams
     Enes100.mission(2.43);
