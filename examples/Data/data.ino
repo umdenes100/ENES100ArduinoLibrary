0c521e67f2b7d6573e69e87631d9d4a4a093a795
alecLahr <79341501+alecLahr@users.noreply.github.com>
2022-02-28 14:05:56 -0500

    changed default TX/RX pins

--- examples/Data/data.ino
+++ examples/Data/data.ino
@@ -3,7 +3,7 @@
 void setup() {
     // Initialize Enes100 Library
     // Team Name, Mission Type, Marker ID, TX Pin, RX Pin
-    Enes100.begin("DATA TEAM", DATA, 3, 8, 9);
+    Enes100.begin("DATA TEAM", DATA, 3, 10, 11);
 
     Enes100.print("Destination is at (");
     Enes100.print(Enes100.destination.x);
